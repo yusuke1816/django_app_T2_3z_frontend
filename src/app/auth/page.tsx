74f157f59324382d5713df7caa18cda665d2bb1d
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-07-10 01:48:26 +0900

    'Accept': 'application/json', 追加

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -20,7 +20,8 @@ export default function LoginPage() {
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/token/`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json',
+        'Accept': 'application/json',  },
         body: JSON.stringify({ username, password }),
       });
 
